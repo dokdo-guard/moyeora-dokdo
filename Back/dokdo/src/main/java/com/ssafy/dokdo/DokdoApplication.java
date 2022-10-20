2146ed1a9de94bff045670d3d70a8c611e34c541
SSAFYCYJ <dakadern@gmail.com>
2022-10-20 12:52:47 +0900

    [BE] -Config:백엔드 프로젝트 생성

---
+++ Back/dokdo/src/main/java/com/ssafy/dokdo/DokdoApplication.java
@@ -0,0 +1,13 @@
+package com.ssafy.dokdo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DokdoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DokdoApplication.class, args);
+	}
+
+}
