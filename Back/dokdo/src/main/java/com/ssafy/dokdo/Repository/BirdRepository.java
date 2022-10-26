caff27272bfbfbfa6ec101fc7ff8d111f748c022
SSAFYCYJ <dakadern@gmail.com>
2022-10-26 10:16:28 +0900

    [BE] - Feat: Quiz API 구현

--- Back/dokdo/src/main/java/com/ssafy/dokdo/Repository/BirdRepository.java
+++ Back/dokdo/src/main/java/com/ssafy/dokdo/Repository/BirdRepository.java
@@ -3,9 +3,10 @@ package com.ssafy.dokdo.Repository;
 import com.ssafy.dokdo.Entity.Bird;
 import com.ssafy.dokdo.Entity.Plant;
 import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
-
+@Repository
 public interface BirdRepository extends MongoRepository<Bird,String> {
     Optional<Bird> findBirdByName(String name);
 }
--- Back/dokdo/src/main/java/com/ssafy/dokdo/Repository/PlantRepository.java
+++ Back/dokdo/src/main/java/com/ssafy/dokdo/Repository/PlantRepository.java
@@ -2,9 +2,10 @@ package com.ssafy.dokdo.Repository;
 
 import com.ssafy.dokdo.Entity.Plant;
 import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
-
+@Repository
 public interface PlantRepository extends MongoRepository<Plant,String> {
     Optional<Plant> findPlantByName(String name);
 }
