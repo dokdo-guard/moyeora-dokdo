0fd7289fa6df56196dcaff927534760064c2bcbe
SSAFYCYJ <dakadern@gmail.com>
2022-10-26 10:35:35 +0900

    [BE]-Config: Context Path 변경 및 Repository Annotation 추가

--- Back/dokdo/src/main/java/com/ssafy/dokdo/Controller/QuizController.java
+++ Back/dokdo/src/main/java/com/ssafy/dokdo/Controller/QuizController.java
@@ -14,7 +14,7 @@ import java.util.List;
 import java.util.Optional;
 
 @RestController
-@RequestMapping
+@RequestMapping("api")
 @AllArgsConstructor
 public class QuizController {
 
--- Back/dokdo/src/main/java/com/ssafy/dokdo/Repository/PlantRepository.java
+++ Back/dokdo/src/main/java/com/ssafy/dokdo/Repository/PlantRepository.java
@@ -5,9 +5,8 @@ import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
-<<<<<<< Back/dokdo/src/main/java/com/ssafy/dokdo/Repository/PlantRepository.java
+
 @Repository
-=======
 
 public interface PlantRepository extends MongoRepository<Plant,String> {
     Optional<Plant> findPlantByName(String name);
