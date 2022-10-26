4406fffccb8d5a2f602d58c3b09fe8a91de273be
sr <hmr971127@gmail.com>
2022-10-26 09:04:25 +0900

    [BE] - Feat : swagger

--- Back/dokdo/src/main/java/com/ssafy/dokdo/Controller/SeaAnimalController.java
+++ Back/dokdo/src/main/java/com/ssafy/dokdo/Controller/SeaAnimalController.java
@@ -3,6 +3,9 @@ package com.ssafy.dokdo.Controller;
 import com.ssafy.dokdo.Entity.SeaAnimal;
 import com.ssafy.dokdo.Entity.SeaPlant;
 import com.ssafy.dokdo.Service.SeaAnimalService;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiParam;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -19,13 +22,16 @@ public class SeaAnimalController {
 
     private final SeaAnimalService seaAnimalService;
 
+    @ApiOperation(value = "해양동물 목록 조회", notes = "해양동물 목록의 상세 정보를 조회한다.")
     @GetMapping("/sea-animals")
     public List<SeaAnimal> getAllSeaAnimals(){
         return seaAnimalService.getAllSeaAnimals();
     }
 
+    @ApiOperation(value = "해양동물 조회", notes = "해양동물 하나의 상세 정보를 조회한다.")
     @GetMapping("/sea-animal")
-    public Optional<SeaAnimal> getSeaAnimal(@RequestParam String name){
+    public Optional<SeaAnimal> getSeaAnimal(
+            @RequestParam @ApiParam("해당 해양동물의 상세 정보를 불러온다.") String name){
         return seaAnimalService.getSeaAnimal(name);
     }
 }
