3cbbd09f029fb2485823085371039a755834c281
sojung13 <bestsojung13@gmail.com>
2022-10-27 10:45:27 +0900

    [FE]-Feat:메인 페이지 구성 및 glTF 파일 업로드

---
+++ Front/dokdo/src/components/glTF/Territory.js
@@ -0,0 +1,25 @@
+import * as THREE from 'three';
+export class Territory {
+	constructor(info) {
+		this.x = info.x;
+		this.y = info.y;
+		this.z = info.z;
+		
+		info.gltfLoader.load(
+			info.modelSrc,
+			glb => {
+				glb.scene.traverse(child => {
+					if (child.isMesh) {
+						child.castShadow = true;
+					} 
+				});
+				this.modelMesh = glb.scene.children[0];
+				this.modelMesh.rotation.y = -1.5;
+				this.modelMesh.castShadow = true;
+				this.modelMesh.position.set(this.x, this.y, this.z);
+				this.modelMesh.scale.multiplyScalar(0.4);
+				info.scene.add(this.modelMesh);
+			}
+		);
+	}
+}
