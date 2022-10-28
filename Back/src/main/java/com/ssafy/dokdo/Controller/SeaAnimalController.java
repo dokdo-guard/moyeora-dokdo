48e33ad4bf6c42fb3c4580e5db75c68ad65e78fa
sr <hmr971127@gmail.com>
2022-10-28 09:26:54 +0900

    [BE] - FEAT : controller 수정

--- Back/src/main/java/com/ssafy/dokdo/Controller/SeaAnimalController.java
+++ Back/src/main/java/com/ssafy/dokdo/Controller/SeaAnimalController.java
@@ -19,13 +19,13 @@ public class SeaAnimalController {
     private final SeaAnimalService seaAnimalService;
 
     @ApiOperation(value = "해양동물 목록 조회", notes = "해양동물 목록의 상세 정보를 조회한다.")
-    @GetMapping("/sea-animals")
+    @GetMapping("sea-animals")
     public List<SeaAnimal> getAllSeaAnimals(){
         return seaAnimalService.getAllSeaAnimals();
     }
 
     @ApiOperation(value = "해양동물 조회", notes = "해양동물 하나의 상세 정보를 조회한다.")
-    @GetMapping("/sea-animal")
+    @GetMapping("sea-animal")
     public Optional<SeaAnimal> getSeaAnimal(
             @RequestParam @ApiParam("해당 해양동물의 상세 정보를 불러온다.") String name){
         return seaAnimalService.getSeaAnimal(name);
