a716ce6959413bcd0fdbf83e71ae7c5559442579
Hyoseon Kim <apso123@naver.com>
2022-10-31 13:47:40 +0900

    [BE] - FEAT : user 캐릭터/닉네임 수정 api 구현

--- Back/src/main/java/com/ssafy/dokdo/Controller/UserController.java
+++ Back/src/main/java/com/ssafy/dokdo/Controller/UserController.java
@@ -5,6 +5,7 @@ import com.ssafy.dokdo.Exception.ResourceNotFoundException;
 import com.ssafy.dokdo.Repository.UserRepository;
 import com.ssafy.dokdo.Security.CurrentUser;
 import com.ssafy.dokdo.Security.UserPrincipal;
+import com.ssafy.dokdo.Service.UserService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -19,7 +20,8 @@ import java.util.Optional;
 @RequiredArgsConstructor
 public class UserController {
 
-    private UserRepository userRepository;
+    private final UserRepository userRepository;
+    private final UserService userService;
 
     @GetMapping("user")
     public User getCurrentUser(@CurrentUser UserPrincipal userPrincipal) {
@@ -39,15 +41,13 @@ public class UserController {
     }
 
     @PutMapping("character")
-    public User setCharacter(@CurrentUser UserPrincipal userPrincipal) {
-        return userRepository.findById(userPrincipal.getId())
-                .orElseThrow(() -> new ResourceNotFoundException("User", "id", userPrincipal.getId()));
+    public User setCharacter(@CurrentUser UserPrincipal userPrincipal, @RequestBody User user) {
+        return userService.updateUserCharacter(userPrincipal.getId(), user.getUserCharacter());
     }
 
     @PutMapping("nickname")
-    public User setNickname(@CurrentUser UserPrincipal userPrincipal) {
-        return userRepository.findById(userPrincipal.getId())
-                .orElseThrow(() -> new ResourceNotFoundException("User", "id", userPrincipal.getId()));
+    public User setNickname(@CurrentUser UserPrincipal userPrincipal, @RequestBody User user) {
+        return userService.updateName(userPrincipal.getId(), user.getName());
     }
 
 }
