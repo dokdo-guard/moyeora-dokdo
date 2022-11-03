7d5856d418f1a624830aed3fa142270a47c65e37
DESKTOP-KVCQHCD\multicampus <dlacogus5239@gamil.com>
2022-11-03 15:33:11 +0900

    [FE] - "" : 임시 커밋

--- Front/src/components/common/LoadingComponent.js
+++ Front/src/components/common/LoadingComponent.js
@@ -2,6 +2,12 @@ import React from "react";
 import "../css/LoadingComponent.css";
 
 function LoadingComponent() {
-  return <div className='LoadingCompoenetContainer'></div>;
+  return (
+    <div className='LoadingCompoenetContainer'>
+      <div className='LoadingTipBox'>TIP</div>
+
+      <div className='LoadingProgressBar'>ProgressBar</div>
+    </div>
+  );
 }
 export default LoadingComponent;
