c56778ec6cee162666e7154909741f09e9ca28c4
siryeongchoi <siryeongchoi@gmail.com>
2022-11-04 09:22:06 +0900

    [FE]-Fix:마이페이지 css 및 이미지 파일 경로 수정

--- Front/src/pages/MypageTest.js
+++ Front/src/pages/MypageTest.js
@@ -1,5 +1,5 @@
 import { useState } from "react";
-import Popup from "../components/mypage/dictionary";
+import Popup from "../components/mypage/badge";
 
 function MypageTest() {
   const [visibility, setVisibility] = useState(true);
