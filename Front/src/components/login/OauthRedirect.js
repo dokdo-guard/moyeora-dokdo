f02103524824a27337b197429eebd617a956f482
DESKTOP-KVCQHCD\multicampus <dlacogus5239@gamil.com>
2022-11-07 10:28:06 +0900

    [FE] - FIX : Prettier Bug Fix

--- Front/src/components/login/OauthRedirect.js
+++ Front/src/components/login/OauthRedirect.js
@@ -1,28 +1,28 @@
-import { useEffect } from 'react';
-import { useNavigate } from 'react-router-dom';
+import { useEffect } from "react";
+import { useNavigate } from "react-router-dom";
 
 const OauthRedirect = (props) => {
   const navigate = useNavigate();
 
   const getUrlParameter = (keyVal) => {
-    keyVal = keyVal.replace(/[\[]/, '\\[').replace(/[\]]/, '\\]');
-    let regex = new RegExp('[\\?&]' + keyVal + '=([^&#]*)');
+    keyVal = keyVal.replace(/[\[]/, "\\[").replace(/[\]]/, "\\]");
+    let regex = new RegExp("[\\?&]" + keyVal + "=([^&#]*)");
 
     let results = regex.exec(window.location);
     return results === null
-      ? ''
-      : decodeURIComponent(results[1].replace(/\+/g, ' '));
+      ? ""
+      : decodeURIComponent(results[1].replace(/\+/g, " "));
   };
 
   useEffect(() => {
-    const token = getUrlParameter('token');
-    const error = getUrlParameter('error');
+    const token = getUrlParameter("token");
+    const error = getUrlParameter("error");
     if (token) {
-      localStorage.setItem('accessToken', token);
-      navigate('/main/mainTest');
+      localStorage.setItem("accessToken", token);
+      navigate("/main/mainTest");
     } else {
       console.log(error);
-      navigate('/login');
+      navigate("/login");
     }
   });
 
