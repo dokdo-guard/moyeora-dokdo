611992c3f9a34dc026214b232ea55952f083ff33
DESKTOP-KVCQHCD\multicampus <dlacogus5239@gamil.com>
2022-11-08 10:50:56 +0900

    [FE] - 임시 커밋

--- Front/src/App.js
+++ Front/src/App.js
@@ -6,15 +6,18 @@ import { PopupTest, MypageTest, MainTest, LoadingTest } from "./pages/index";
 import Login from "./components/login/Login";
 import OauthRedirect from "./components/login/OauthRedirect";
 
+// import { useSelector } from "react-redux";
+
 function App() {
+  // const user = useSelector((state) => state.user.value);
   return (
     <>
       <Routes>
+        <Route path='/' element={<Login />} />
         <Route path='/popup/popupTest' element={<PopupTest />} />
         <Route path='/mypage/mypageTest' element={<MypageTest />} />
         <Route path='/main/mainTest' element={<MainTest />} />
         <Route path='/LoadingTest' element={<LoadingTest />} />
-        <Route path='/login' element={<Login />} />
         <Route path='/oauth/redirect' element={<OauthRedirect />} />
       </Routes>
     </>
