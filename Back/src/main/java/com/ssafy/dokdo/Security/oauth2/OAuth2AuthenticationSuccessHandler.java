5ba5c061603d6d810ee7ae84d1356c2e39fdfad0
Hyoseon Kim <apso123@naver.com>
2022-11-10 14:35:18 +0900

    [BE] - Refactor: @Slf4j 추가

--- Back/src/main/java/com/ssafy/dokdo/Security/oauth2/OAuth2AuthenticationSuccessHandler.java
+++ Back/src/main/java/com/ssafy/dokdo/Security/oauth2/OAuth2AuthenticationSuccessHandler.java
@@ -4,6 +4,7 @@ import com.ssafy.dokdo.Config.AppProperties;
 import com.ssafy.dokdo.Exception.BadRequestException;
 import com.ssafy.dokdo.Security.TokenProvider;
 import com.ssafy.dokdo.Util.CookieUtils;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.web.authentication.SimpleUrlAuthenticationSuccessHandler;
@@ -20,6 +21,7 @@ import java.util.Optional;
 
 import static com.ssafy.dokdo.Security.oauth2.HttpCookieOAuth2AuthorizationRequestRepository.REDIRECT_URI_PARAM_COOKIE_NAME;
 
+@Slf4j
 @Component
 public class OAuth2AuthenticationSuccessHandler extends SimpleUrlAuthenticationSuccessHandler {
 
@@ -43,7 +45,7 @@ public class OAuth2AuthenticationSuccessHandler extends SimpleUrlAuthenticationS
         String targetUrl = determineTargetUrl(request, response, authentication);
 
         if (response.isCommitted()) {
-            logger.debug("Response has already been committed. Unable to redirect to " + targetUrl);
+            log.debug("Response has already been committed. Unable to redirect to " + targetUrl);
             return;
         }
 
