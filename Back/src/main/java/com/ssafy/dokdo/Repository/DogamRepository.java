f3b0323824b1583f02889d12005c7b7dc7f078b0
Hyoseon Kim <apso123@naver.com>
2022-11-11 16:46:46 +0900

    Revert "[BE] - Fix: import 오류 삭제"
    This reverts commit 1dc685601029ce5d9fbedec9bfc54db65c4b8e5a.

--- Back/src/main/java/com/ssafy/dokdo/Repository/DogamRepository.java
+++ Back/src/main/java/com/ssafy/dokdo/Repository/DogamRepository.java
@@ -1,5 +1,6 @@
 package com.ssafy.dokdo.Repository;
 
+import com.ssafy.dokdo.Entity.Badge;
 import com.ssafy.dokdo.Entity.Dogam;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
