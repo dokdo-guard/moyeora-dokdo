cf5f9b147b15464151591a41b858cdd09a3505e6
DESKTOP-KVCQHCD\multicampus <dlacogus5239@gamil.com>
2022-11-14 18:01:06 +0900

    [FE] - FIX : 진행상황까지 커밋

--- Front/src/components/login/Login.js
+++ Front/src/components/login/Login.js
@@ -1,13 +1,44 @@
-import { React, useState } from "react";
+import { React, useState, forwardRef } from "react";
 import {
   GOOGLE_AUTH_URL,
   NAVER_AUTH_URL,
   KAKAO_AUTH_URL,
 } from "../../api/Oauth/uri";
 import "../css/Login.css";
+import Dialog from "@mui/material/Dialog";
+import DialogActions from "@mui/material/DialogActions";
+import DialogContent from "@mui/material/DialogContent";
+import DialogTitle from "@mui/material/DialogTitle";
+import Slide from "@mui/material/Slide";
+import HelpOutlineIcon from "@mui/icons-material/HelpOutline";
+import Icon from "@mui/material/Icon";
+const style = {
+  position: "absolute",
+  top: "50%",
+  left: "50%",
+  transform: "translate(-50%, -50%)",
+  width: 400,
+  bgcolor: "background.paper",
+  border: "2px solid #000",
+  boxShadow: 24,
+  pt: 2,
+  px: 4,
+  pb: 3,
+};
+const Transition = forwardRef(function Transition(props, ref) {
+  return <Slide direction='up' ref={ref} {...props} />;
+});
 
 function Login(props) {
   const [loginBtnShow, setLoginBtnShow] = useState(false);
+  const [open, setOpen] = useState(false);
+  const handleOpen = () => {
+    setOpen(true);
+  };
+  const handleClose = () => {
+    setOpen(false);
+  };
+
   const OnBoard = () => {
     return (
       <div className='socialLogin'>
@@ -26,6 +57,32 @@ function Login(props) {
   return (
     <div className='content'>
       <div className='LoginWrapper'>
+        <div className='WhatIsOurServiceBtn' onClick={handleOpen}>
+          <HelpOutlineIcon />
+          모여봐요 우리땅 독도? 이게 뭐야?
+        </div>
+        <Dialog
+          open={open}
+          TransitionComponent={Transition}
+          keepMounted
+          onClose={handleClose}
+          aria-describedby='alert-dialog-slide-description'
+        >
+          <DialogTitle>
+            <div>{"모여봐요 우리땅 독도는 어떤 사이트인가요?"}</div>
+          </DialogTitle>
+          <DialogContent>
+            <div style={{ fontWeight: "300" }}>
+              모여봐요 우리땅 독도(모우독)은 우리 땅 독도를 다양한 컨텐츠로
+              알아갈 수 있는 웹서비스입니다. 캐릭터를 움직여 서도, 동도를 오가며
+              다양한 컨텐츠를 즐기며 독도에 대해 더 깊이 알아갈 수 있는 시간을
+              가져봐요!
+            </div>
+          </DialogContent>
+          <DialogActions>
+            <button onClick={handleClose}>넵 ! 잘 알겠어요</button>
+          </DialogActions>
+        </Dialog>
         <div className='LoginTitle waviy'>
           <div style={{ "--i": 1 }}>
             <h1>모</h1>
@@ -60,6 +117,7 @@ function Login(props) {
             <h1>도</h1>
           </div>
         </div>
+
         <div style={{ textAlign: "center" }}>
           디스플레이 125% 크롬 100%환경에서 최적의 경험을 제공합니다.
         </div>
