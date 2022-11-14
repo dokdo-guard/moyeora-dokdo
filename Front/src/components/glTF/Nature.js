d7c048c14b5d6613eabc7eede2f91a753797c05e
sojung13 <bestsojung13@gmail.com>
2022-11-14 16:20:07 +0900

    [FE]-Feat:그림자 삭제 및 gltf 일부 용량 줄임

--- Front/src/components/glTF/Nature.js
+++ Front/src/components/glTF/Nature.js
@@ -9,11 +9,11 @@ export class Nature {
 		info.gltfLoader.load(
 			info.modelSrc,
 			glb => {
-				glb.scene.traverse(child => {
-					if (child.isMesh) {
-						child.castShadow = true;
-					} 
-				});
+				// glb.scene.traverse(child => {
+				// 	if (child.isMesh) {
+				// 		child.castShadow = true;
+				// 	} 
+				// });
 				this.modelMesh = glb.scene;
 				this.modelMesh.castShadow = true;
 				this.modelMesh.position.set(this.x, this.y, this.z);
