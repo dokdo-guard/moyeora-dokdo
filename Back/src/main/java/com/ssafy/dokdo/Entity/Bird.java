e58de1e022cd29d4f453df2001bf32f761269372
sr <hmr971127@gmail.com>
2022-11-16 14:05:17 +0900

    [BE] - FEAT : random dogam api 수정

--- Back/src/main/java/com/ssafy/dokdo/Entity/Bird.java
+++ Back/src/main/java/com/ssafy/dokdo/Entity/Bird.java
@@ -16,6 +16,7 @@ public class Bird {
     private String summary;
     private String speciesInformation;
     private String img;
+    private String domain;
 
 
 }
--- Back/src/main/java/com/ssafy/dokdo/Entity/SeaAnimal.java
+++ Back/src/main/java/com/ssafy/dokdo/Entity/SeaAnimal.java
@@ -18,4 +18,5 @@ public class SeaAnimal {
     private String summary;
     private String speciesInformation;
     private String img;
+    private String domain;
 }
