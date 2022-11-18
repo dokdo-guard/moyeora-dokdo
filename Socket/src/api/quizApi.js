82ec9dbd2d7e6fe63e0d74b7a1852a1cdc0cd8b7
choiyoungjin <ip951122@naver.com>
2022-11-18 10:37:25 +0900

    [ALL] - DOCS 포팅메뉴얼 작성

--- Socket/src/api/quizApi.js
+++
@@ -1,5 +0,0 @@
-import api from "./api";
-
-export const getQuiz = async (number, success, fail) => {
-  return await api.get(`/info/quiz?number=${number}`).then(success).catch(fail);
-};
--- Socket/src/components/index.js
+++
@@ -1,5 +0,0 @@
-export { default as OXQuizPopup } from "./popup/OXQuizPopup";
-export { default as TerrianPopup } from "./popup/TerrianPopup";
-export { default as EcoSystemPopup } from "./popup/EcosystemPopup";
-export { default as HistoryPopup } from "./popup/HistoryPopup";
-export { default as LoadingComponent } from "./common/LoadingComponent";
